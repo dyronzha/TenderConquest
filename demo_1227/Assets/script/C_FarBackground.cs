b574549d556675721ab655ca1f721d80d1d51e20
MSI\Dyron <dyron85511@gmail.com>
2017-04-17 10:07:25 +0800

    新增遠景

--- demo_1227/Assets/script/C_FarBackground.cs
+++ demo_1227/Assets/script/C_FarBackground.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class C_FarBackground : MonoBehaviour {
 
     float bk_f_x,bk_f_y;
-    GameObject player;
-    float limit_r, limit_l, player_r, player_l,trans_value;
-    float limit_t, limit_b, player_t, player_b,trans_value_y;
+    GameObject camera;
+    float limit_r, limit_l, camera_r, camera_l, trans_value;
+    float limit_t, limit_b, camera_t, camera_b, trans_value_y;
     // Use this for initialization
     void Awake () {
-        player = GameObject.Find("Player");
-        limit_l = -4.6f; limit_r = 38.5f; player_l = 14.5f; player_r = 94.0f;
-        limit_t = 16.5f; limit_b = 13.5f; player_t = 22.65f; player_b = 18.5f;
-        trans_value = (limit_r - limit_l)/(player_r - player_l);
+        camera = GameObject.Find("Main Camera");
+        limit_l = 37.4f; limit_r = 64.5f; camera_l = 20.4f; camera_r = 89.0f;
+        limit_t = 25.2f; limit_b = 17.6f; camera_t = 27.0f; camera_b = 16.0f;
+        trans_value = (limit_r - limit_l)/(camera_r - camera_l);
         bk_f_x = limit_l;
         bk_f_y = limit_b;
-        trans_value_y = (limit_t - limit_b) / (player_t - player_b);
+        trans_value_y = (limit_t - limit_b) / (camera_t - camera_b);
     }
 	
 	// Update is called once per frame
@@ -26,18 +26,18 @@ public class C_FarBackground : MonoBehaviour {
 
     void FarBackgroundMove()
     {
-        if (player.transform.position.x <= player_l) bk_f_x = limit_l;
-        else if (player.transform.position.x >= player_r) bk_f_x = limit_r;
+        if (camera.transform.position.x <= camera_l) bk_f_x = limit_l;
+        else if (camera.transform.position.x >= camera_r) bk_f_x = limit_r;
         else
         {
-                bk_f_x = limit_l + (player.transform.position.x - player_l) * trans_value;
+                bk_f_x = limit_l + (camera.transform.position.x - camera_l) * trans_value;
         }
 
-        if (player.transform.position.y <= player_b) bk_f_y = limit_b;
-        else if (player.transform.position.y >= player_t) bk_f_y = limit_t;
+        if (camera.transform.position.y <= camera_b) bk_f_y = limit_b;
+        else if (camera.transform.position.y >= camera_t) bk_f_y = limit_t;
         else
         {
-            if(player.transform.localScale.y>0)bk_f_y = limit_b + (player.transform.position.y - player_b) * trans_value_y;
+            if(camera.transform.localScale.y>0)bk_f_y = limit_b + (camera.transform.position.y - camera_b) * trans_value_y;
         }
         transform.position = new Vector3(bk_f_x, bk_f_y, transform.position.z);
     }
